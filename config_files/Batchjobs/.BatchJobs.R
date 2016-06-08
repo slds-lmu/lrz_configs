65ebd82eadc8f86b134af9295aad1534f7dd169a
janek <janek.thomas@web.de>
2016-06-08 21:04:50 +0200

    symbolic links to batch config files

--- config_files/Batchjobs/.BatchJobs.R
+++ config_files/Batchjobs/.BatchJobs.R
@@ -1,3 +1,3 @@
-cluster.functions = makeClusterFunctionsSLURM(template.file = "lmu_lrz.tmpl", 
+cluster.functions = makeClusterFunctionsSLURM(template.file = "/home/hpc/ua341/di25koz/lrz_configs/config_files/batchjobs/lmu_lrz.tmpl", 
 	list.jobs.cmd = c("squeue", "-h", "-o %i", "-u $USER", 
 	                  "--clusters=serial", "| tail -n +2"))
--- config_files/batchjobs/.batchtools.conf.r
+++ config_files/batchjobs/.batchtools.conf.r
@@ -1,5 +1,5 @@
-cluster.functions = batchtools::makeClusterFunctionsSLURM("~/lmu_lrz_new.tmpl", clusters = 
-"mpp2")
+cluster.functions = batchtools::makeClusterFunctionsSLURM("/home/hpc/ua341/di25koz/lrz_configs/config_files/batchjobs/lmu_lrz_new.tmpl", 
+	clusters = "mpp2")
 default.resources = list(walltime = 300L, memory = 512L, ntasks = 28L)
 
 
