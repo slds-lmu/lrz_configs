2260e5fb3b4bea5da17df2b2db546727b53e3c13
Janek Thomas <di25koz@mpp2-login5.cos.lrz.de>
2016-06-27 14:50:41 +0200

    use mpp2 or serial as default cluster, depending on ntask

--- config_files/batchtools/.batchtools.conf.r
+++ config_files/batchtools/.batchtools.conf.r
@@ -1,5 +1,2 @@
-cluster.functions = batchtools::makeClusterFunctionsSLURM("/home/hpc/ua341/di25koz/lrz_configs/config_files/batchjobs/lmu_lrz_new.tmpl", 
-	clusters = "mpp2")
-default.resources = list(walltime = 300L, memory = 512L, ntasks = 28L)
-
-debug = TRUE
+cluster.functions = batchtools::makeClusterFunctionsSLURM("/home/hpc/ua341/di25koz/lrz_configs/config_files/batchtools/slurm_lmulrz.tmpl")
+default.resources = list(walltime = 300L, memory = 512L, ntasks = 1L)
