d209e46b73e58412689b4b7c391ddd23680f10af
Janek Thomas <ri89coc2@lxa191.cos.lrz.de>
2016-07-11 21:58:28 +0200

    fix missing clusters, so that the cluster functions now work

--- config_files/batchtools/.batchtools.conf.R
+++ config_files/batchtools/.batchtools.conf.R
@@ -1,2 +1,4 @@
-cluster.functions = batchtools::makeClusterFunctionsSLURM("/home/hpc/pr74ze/ri89coc2/lrz_configs/config_files/batchtools/slurm_lmulrz.tmpl")
-default.resources = list(walltime = 300L, memory = 512L, ntasks = 1L, max.concurrent.jobs = 1000L)
+cluster.functions = batchtools::makeClusterFunctionsSLURM("/home/hpc/pr74ze/ri89coc2/lrz_configs/config_files/batchtools/slurm_lmulrz.tmpl", clusters = "serial")
+default.resources = list(walltime = 300L, memory = 512L, ntasks = 1L)
+
+max.concurrent.jobs = 1000L
