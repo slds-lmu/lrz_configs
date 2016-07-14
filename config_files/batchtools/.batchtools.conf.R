63a87db8531b7d5ec695102b82ac17721cb492d0
Janek Thomas <ri89coc2@lxa191.cos.lrz.de>
2016-07-14 13:35:30 +0200

    change name of cluster function since last batchtools update

--- config_files/batchtools/.batchtools.conf.R
+++ config_files/batchtools/.batchtools.conf.R
@@ -1,4 +1,5 @@
-cluster.functions = batchtools::makeClusterFunctionsSLURM("/home/hpc/pr74ze/ri89coc2/lrz_configs/config_files/batchtools/slurm_lmulrz.tmpl", clusters = "serial")
+cluster.functions = batchtools::makeClusterFunctionsSlurm("/home/hpc/pr74ze/ri89coc2/lrz_configs/config_files/batchtools/slurm_lmulrz.tmpl", 
+clusters = "serial")
 default.resources = list(walltime = 300L, memory = 512L, ntasks = 1L)
 
 max.concurrent.jobs = 1000L
