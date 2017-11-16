8f18a06c9e691174d1b73601e3eb7eef3be552e3
Janek Thomas <ri89coc2@mpp2-login5.cos.lrz.de>
2017-11-16 13:45:23 +0100

    fix array jobs

--- config_files/batchtools/.batchtools_stat.conf.R
+++ config_files/batchtools/.batchtools_stat.conf.R
@@ -1,4 +1,4 @@
-cluster.functions = batchtools::makeClusterFunctionsSlurm("~/lrz_configs/config_files/batchtools/slurm_lmulrz.tmpl")
+cluster.functions = batchtools::makeClusterFunctionsSlurm("~/lrz_configs/config_files/batchtools/slurm_lmulrz.tmpl", array.jobs = FALSE)
 default.resources = list(walltime = 300L, memory = 512L, ntasks = 1L, ncpus = 1L, nodes = 1L, clusters = "serial")
 
 max.concurrent.jobs = 1000L
