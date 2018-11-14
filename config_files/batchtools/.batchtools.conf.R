5b1c47c59d52475c0cdcba2cfc5e0f84d8a8da93
Janek Thomas <janek.thomas@web.de>
2018-11-14 16:27:24 +0100

    ...

--- config_files/batchtools/.batchtools.conf.R
+++ config_files/batchtools/.batchtools.conf.R
@@ -2,4 +2,4 @@ source("/home/hpc/pr74ze/ri89coc2/lrz_configs/config_files/batchtools/clusterFun
 cluster.functions = makeClusterFunctionsSlurmLrz("/home/hpc/pr74ze/ri89coc2/lrz_configs/config_files/batchtools/slurm_lmulrz.tmpl", array.jobs = FALSE)
 default.resources = list(walltime = 300L, memory = 512L, ntasks = 1L, ncpus = 1L, nodes = 1L, clusters = "serial")
 
-max.concurrent.jobs = 1000L
+max.concurrent.jobs = 999L 
