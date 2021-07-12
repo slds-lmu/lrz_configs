98ca484b8980d4f8480bf1dcbd90a74aea50d33b
Giuseppe Casalicchio <giuseppe.casalicchio@stat.uni-muenchen.de>
2021-07-12 17:31:22 +0200

    Update .batchtools.conf.R
    In https://doku.lrz.de/display/PUBLIC/Job+Processing+on+the+Linux-Cluster, it says that 250 is the submission limit.

--- config_files/batchtools/.batchtools.conf.R
+++ config_files/batchtools/.batchtools.conf.R
@@ -2,4 +2,4 @@ sys_home_dir = system("echo $HOME", intern = TRUE)
 source(paste0(sys_home_dir, "/lrz_configs/config_files/batchtools/clusterFunctionsSlurmLrz.R"))
 cluster.functions = makeClusterFunctionsSlurmLrz(paste0(sys_home_dir, "/lrz_configs/config_files/batchtools/slurm_lmulrz.tmpl"), array.jobs = FALSE)
 default.resources = list(walltime = 300L, memory = 512L, ntasks = 1L, ncpus = 1L, nodes = 1L, clusters = "serial")
-max.concurrent.jobs = 999L
+max.concurrent.jobs = 249L
